beaca02c64a24c59b905326a7c0f9327da9cb800
DavidPokrajac <d0pokrajac@gmail.com>
2020-10-10 15:31:25 +0200

    Added final event listener

--- sandbox.js
+++ sandbox.js
@@ -5,7 +5,7 @@ window.onload = function(){
 
     var form = document.forms[0];
 
-    var input = form.querySelector("input");
+    // var input = form.querySelector("input");
 
     var ul = document.querySelector("ul");
 
@@ -27,22 +27,50 @@ window.onload = function(){
         
         e.preventDefault();
 
-        var value = input.value.trim();
+        var value = form.item.value.toLowerCase().trim();
 
         /* If the value of the input tag is more than 1 in length, call generate function and pass value of the input tag as the argment.
         Also, reset the form after submitting. If there's no length, make a p tag that will be inserted as an alert to type somehting,
         and then disappear after certain amount of seconds */
         if(value.length >= 1){
             generate(value);
-            form.reset();
+            form.item.value = "";
         } else{
             var p = document.createElement("p");
             p.textContent = "Please Enter Valid Value";
+            p.classList.add("alerted");
             mainContainer.prepend(p);
             setTimeout(function(){
                 mainContainer.removeChild(p);
             }, 4000);
         }
+    });
+
+    ul.addEventListener("click", function(e){
+
+        e.stopPropagation();
+
+        if(e.target.classList.contains("delete")){
+            e.target.parentNode.parentNode.remove();
+        }
+
+        if(e.target.classList.contains("edit")){
+            e.target.parentNode.parentNode.style.display = "none";
+            form.item.value = e.target.parentNode.parentNode.textContent;
+        }
+
+        if(e.target.classList.contains("completed")){
+            e.target.parentNode.parentNode.style.color = "grey";
+            e.target.parentNode.parentNode.style.textDecoration = "line-through";
+            e.target.style.opacity = "0.2";
+            e.target.classList.remove("completed");
+        } else{
+            e.target.parentNode.parentNode.style.color = "black";
+            e.target.parentNode.parentNode.style.textDecoration = "none";
+            e.target.style.opacity = "1";
+            e.target.classList.add("completed");
+        }
+
     });
 
     // Event listener to be called when all of the items want to be deleted at once
